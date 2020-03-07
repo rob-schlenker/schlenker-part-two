1c68d8c8c1c555eccdd18f19f265dfa01f899236
rob <schlenk7@gmail.com>
2020-03-06 23:02:46 -0500

    First commit for deliverable two

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+var vacType = prompt("What kind of trip would you like to go on, musical, tropical, or adventurous?");
+var groupSize = prompt("How many are in your group?");
+let travel = "";
+let destination = "";
+
+
+if (vacType === "musical") {
+  destination = "New Orleans";
+} else if (vacType === "tropical") {
+    destination = "Beach Vacation in Mexico";
+} else {
+  destination = "White Water Wafting the Grand Canyon";
+}
+
+if (groupSize < 2) {
+  travel = "First Class";
+} else if (groupSize < 10) {
+    travel = "Helicopter";
+} else {
+  travel = "Charter Flight";
+
+  
+}
+
+var result = ("Since you’re a group of " + groupSize + " going on a " + vacType + " vacation, you should take a " + travel + " to " + destination)
+
+console.log(result)
+
+
+
